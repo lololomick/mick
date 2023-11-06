4ed890ffa5a648354aa4dcb51db4c58c9d523172
lololomick <lololomick@gmail.com>
2023-11-06 15:00:45 +0100

    update

--- webshop/js/scripts.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener("DOMContentLoaded", function () {
-    setTimeout(function() {
-        var preloader = document.querySelector(".preloader");
-        preloader.classList.add('preloader-hidden');
-        preloader.addEventListener('transitionend', function() {
-            preloader.style.display = 'none';
-        });
-    }, 1500);
-});
